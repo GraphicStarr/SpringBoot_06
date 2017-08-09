88eca8137170a19e1880da49a37ad965861f6c81
GraphicStarr <jerilynmensah@gmail.com>
2017-08-09 09:21:26 -0400

    First commit- August 9, 2017 at 9:21am

---
+++ src/main/java/me/jerilynmensah/demo/Song.java
@@ -0,0 +1,10 @@
+package me.jerilynmensah.demo;
+
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+}
